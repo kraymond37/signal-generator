ef37b9c70e85330d512e2b85da7187471bfe48bf
Raymond.K <kraymond37@protonmail.com>
2020-02-12 11:53:09 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/kraymond37/signal-generator
+
+go 1.12
+
+require (
+	github.com/RichardKnop/machinery v1.7.3
+	github.com/google/uuid v1.1.1
+	github.com/opentracing/opentracing-go v1.1.0
+	github.com/sirupsen/logrus v1.4.2
+)
---
+++ sendsignal.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/RichardKnop/machinery/v1"
+	"github.com/RichardKnop/machinery/v1/config"
+	"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/google/uuid"
+	"github.com/opentracing/opentracing-go"
+	opentracinglog "github.com/opentracing/opentracing-go/log"
+	"time"
+)
+
+var server *machinery.Server
+
+func main() {
+	if server = startUpService(); server == nil {
+		return
+	}
+	fmt.Println("start service")
+
+	if err := startTradeFollower(); err != nil {
+		return
+	}
+	fmt.Println("start follower")
+
+	if err := startTradeMonitor(); err != nil {
+		return
+	}
+	fmt.Println("start monitor")
+
+	//time.Sleep(5 * time.Second)
+	//sendTradeSignalToFollower()
+}
+
+func startUpService() *machinery.Server{
+	cfg, err := config.NewFromYaml("robot.yaml", true)
+	if err != nil {
+		fmt.Println(cfg, err)
+		return nil
+	}
+
+	server, err := machinery.NewServer(cfg)
+	if err != nil {
+		fmt.Println(cfg, err)
+		return nil
+	}
+
+	return server
+}
+
+func startTradeMonitor() error {
+	targetInfo := map[string]interface{}{
+		"target_account": "target",
+		"follow_account": "follow",
+		"exchange":       "bitmex",
+		"contracts": []map[string]interface{}{
+			{
+				"symbol":              "XBTUSD",
+				"follow_position_max": int64(1),
+				"follow_position_min": int64(1),
+				"follow_rate":         1.0,
+			},
+		},
+		"endpoint":   "testnet.bitmex.com",
+		"api_key":    "cihb_hjaTRycwJ9ML4UEIGgR",
+		"secret_key": "ZTbKCf47aftV6PvoX7QFvU1EX6wsdzsAc7L-_K-JjHrIrxwv",
+	}
+
+	data, err := json.Marshal(targetInfo)
+	if err != nil {
+		return err
+	}
+
+	var startTradeMonitor = tasks.Signature{
+		Name: "startTradeMonitor",
+		Args: []tasks.Arg{
+			{
+				Type:  "[]byte",
+				Value: data,
+			},
+		},
+	}
+
+	span, ctx := opentracing.StartSpanFromContext(context.Background(), "send")
+	defer span.Finish()
+
+	batchID := "Q" + uuid.New().String()
+	span.SetBaggageItem("batch.id", batchID)
+	span.LogFields(opentracinglog.String("batch.id", batchID))
+
+	return sendTask(ctx, startTradeMonitor)
+}
+
+func startTradeFollower() error {
+	followerInfo := map[string]interface{}{
+		"target_account": "target",
+		"follow_account": "follow",
+		"exchange":       "bitmex",
+		"contracts": []map[string]interface{}{
+			{
+				"symbol":              "XBTUSD",
+				"follow_position_max": int64(10),
+				"follow_position_min": int64(1),
+				"follow_rate":         1.0,
+			},
+		},
+		"endpoint":   "testnet.bitmex.com",
+		"api_key":    "cihb_hjaTRycwJ9ML4UEIGgR",
+		"secret_key": "ZTbKCf47aftV6PvoX7QFvU1EX6wsdzsAc7L-_K-JjHrIrxwv",
+	}
+	followInfos := []map[string]interface{}{followerInfo}
+
+	data, err := json.Marshal(followInfos)
+	if err != nil {
+		return err
+	}
+
+	var startFollower = tasks.Signature{
+		Name: "startTradeFollower",
+		Args: []tasks.Arg{
+			{
+				Type:  "[]byte",
+				Value: data,
+			},
+		},
+	}
+	span, ctx := opentracing.StartSpanFromContext(context.Background(), "send")
+	defer span.Finish()
+
+	batchID := uuid.New().String()
+	span.SetBaggageItem("batch.id", batchID)
+	span.LogFields(opentracinglog.String("batch.id", batchID))
+	return sendTask(ctx, startFollower)
+}
+
+func sendTradeSignalToFollower() {
+	params := map[string]interface{}{
+		"symbol": "XBTUSD",
+		"side": "Buy",
+		"amount": int64(1),
+		"timestamp": time.Now().Unix(),
+		"target_account": "target",
+		"exchange": "bitmex",
+	}
+	signal, _ := json.Marshal(params)
+
+	var task = tasks.Signature{
+		Name: "broadcastTradeSignal",
+		Args: []tasks.Arg{
+			{
+				Type:  "[]byte",
+				Value: signal,
+			},
+		},
+	}
+	span, ctx := opentracing.StartSpanFromContext(context.Background(), "send")
+	defer span.Finish()
+
+	batchID := "Q" + uuid.New().String()
+	span.SetBaggageItem("batch.id", batchID)
+	span.LogFields(opentracinglog.String("batch.id", batchID))
+	_ = sendTask(ctx, task)
+}
+
+func sendTask(ctx context.Context, signature tasks.Signature) error {
+	_, err := server.SendTaskWithContext(ctx, &signature)
+	if err != nil {
+		return fmt.Errorf("could not send task: %s", err.Error())
+	}
+	fmt.Println("send task")
+
+	//results, err := asyncResult.Get(time.Millisecond * 5)
+	//if err != nil {
+	//	return fmt.Errorf("getting task result failed with error: %s", err.Error())
+	//}
+	//log.Println(tasks.HumanReadableResults(results))
+	return nil
+}
