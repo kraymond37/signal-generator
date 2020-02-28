de6f029c455422992a63c9c332471a2e608ed130
Raymond.K <kraymond37@protonmail.com>
2020-02-28 20:29:41 +0800

    更新

--- sendsignal.go
+++ sendsignal.go
@@ -21,10 +21,11 @@ func main() {
 	}
 	fmt.Println("start service")
 
-	if err := startTradeFollower(); err != nil {
-		return
-	}
-	fmt.Println("start follower")
+	//if err := startTradeFollower(); err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println("start follower")
 
 	//if err := startTradeMonitor(); err != nil {
 	//	return
@@ -51,15 +52,19 @@ func startUpService() *machinery.Server {
 }
 
 func startTradeMonitor() error {
+	// bm3
 	targetInfo := map[string]interface{}{
-		"target_account": "target",
-		"follow_account": "follow",
+		"target_account": "bm3",
 		"exchange":       "bitmex",
 		"base_host":      "testnet.bitmex.com",
 		"ws_host":        "testnet.bitmex.com",
-		"access_key":     "F8vOWWaSt2olHd8rUNGnt1gQ",
-		"secret_key":     "15buQgLJDoCaSmXLA9TxZey-9f3S1ybqbbeIj09VGDcBkm55",
+		"access_key":     "jAwOyiesXELlb-lHi3j3zhkp",
+		"secret_key":     "ydYMBfO0dYcFwDonwGZuwyJ3debrLJMonkL23BNLFZC8mxJZ",
 		"symbols":        []interface{}{"XBTUSD"},
+		"ding": map[string]interface{}{
+			"ding":  "",
+			"phone": "",
+		},
 	}
 
 	data, err := json.Marshal(targetInfo)
@@ -89,31 +94,33 @@ func startTradeMonitor() error {
 }
 
 func startTradeFollower() error {
+	// bm4 F8vOWWaSt2olHd8rUNGnt1gQ 15buQgLJDoCaSmXLA9TxZey-9f3S1ybqbbeIj09VGDcBkm55
+	// bm5 9U870JJgfMDFo2Vip6xqlIUg 9sTMUVJv2KeLkA1FkYHa3q8t7K3Sfn_FgM6iHCCT-NCIYLcN
 	followerInfo := map[string]interface{}{
 		"task_title":     "hi",
-		"target_account": "target",
-		"follow_account": "follow",
+		"target_account": "bm3",
+		"follow_account": "bm5",
 		"exchange":       "bitmex",
 		"contracts": []map[string]interface{}{
 			{
-				"id":             "83",
+				"id":             "105",
 				"task_title":     "hi",
 				"contract":       "XBTUSD",
-				"buy_limit_max":  "10",
+				"buy_limit_max":  "1000",
 				"buy_limit_min":  "0",
-				"sell_limit_max": "10",
+				"sell_limit_max": "1000",
 				"sell_limit_min": "0",
 				"price_offset":   "2",
-				"cancel_time":    "5000",
+				"cancel_time":    "5",
 				"follow_rate":    "1.0",
 			},
 		},
 		"base_host":         "testnet.bitmex.com",
 		"ws_host":           "testnet.bitmex.com",
-		"access_key":        "F8vOWWaSt2olHd8rUNGnt1gQ",
-		"secret_key":        "15buQgLJDoCaSmXLA9TxZey-9f3S1ybqbbeIj09VGDcBkm55",
-		"target_access_key": "9U870JJgfMDFo2Vip6xqlIUg",
-		"target_secret_key": "9sTMUVJv2KeLkA1FkYHa3q8t7K3Sfn_FgM6iHCCT-NCIYLcN",
+		"access_key":        "9U870JJgfMDFo2Vip6xqlIUg",
+		"secret_key":        "9sTMUVJv2KeLkA1FkYHa3q8t7K3Sfn_FgM6iHCCT-NCIYLcN",
+		"target_access_key": "jAwOyiesXELlb-lHi3j3zhkp",
+		"target_secret_key": "ydYMBfO0dYcFwDonwGZuwyJ3debrLJMonkL23BNLFZC8mxJZ",
 		"ding": map[string]interface{}{
 			"ding":  "",
 			"phone": "",
@@ -150,8 +157,8 @@ func sendTradeSignalToFollower() {
 		"symbol":              "XBTUSD",
 		"side":                "Buy",
 		"amount":              int64(1),
-		"timestamp":           time.Now().Unix(),
-		"target_account":      "target",
+		"timestamp":           time.Now(),
+		"target_account":      "bm3",
 		"exchange":            "bitmex",
 		"order_id":            "6868686868",
 		"order_status":        "Filled",
