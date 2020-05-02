fde5da861c4b95ae7d2ace7b09b911786f7a2218
Raymond.K <kraymond37@protonmail.com>
2020-05-02 13:46:30 +0800

    更新

--- sendsignal.go
+++ sendsignal.go
@@ -10,6 +10,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/opentracing/opentracing-go"
 	opentracinglog "github.com/opentracing/opentracing-go/log"
+	"log"
 	"time"
 )
 
@@ -34,6 +35,8 @@ func main() {
 	//fmt.Println("start follower")
 
 	//if err := startTradeMonitor(); err != nil {
+	//	fmt.Println(err)
+	//	_ = stopTradeFollower()
 	//	return
 	//}
 	//fmt.Println("start monitor")
@@ -68,8 +71,8 @@ func startTradeMonitor() error {
 		"secret_key":     "ydYMBfO0dYcFwDonwGZuwyJ3debrLJMonkL23BNLFZC8mxJZ",
 		"symbols":        []interface{}{"XBTUSD"},
 		"ding": map[string]interface{}{
-			"ding":  "",
-			"phone": "",
+			"ding":   "",
+			"phones": "",
 		},
 	}
 
@@ -100,39 +103,7 @@ func startTradeMonitor() error {
 }
 
 func startTradeFollower() error {
-	// bm4 F8vOWWaSt2olHd8rUNGnt1gQ 15buQgLJDoCaSmXLA9TxZey-9f3S1ybqbbeIj09VGDcBkm55
-	// bm5 9U870JJgfMDFo2Vip6xqlIUg 9sTMUVJv2KeLkA1FkYHa3q8t7K3Sfn_FgM6iHCCT-NCIYLcN
-	followerInfo := map[string]interface{}{
-		"task_title":     "hi",
-		"target_account": "bm3",
-		"follow_account": "bm5",
-		"exchange":       "bitmex",
-		"contracts": []map[string]interface{}{
-			{
-				"id":             "105",
-				"task_title":     "hi",
-				"contract":       "XBTUSD",
-				"buy_limit_max":  "1000",
-				"buy_limit_min":  "0",
-				"sell_limit_max": "1000",
-				"sell_limit_min": "0",
-				"price_offset":   "2",
-				"cancel_time":    "5",
-				"follow_rate":    "1.0",
-			},
-		},
-		"base_host":         "testnet.bitmex.com",
-		"ws_host":           "testnet.bitmex.com",
-		"access_key":        "9U870JJgfMDFo2Vip6xqlIUg",
-		"secret_key":        "9sTMUVJv2KeLkA1FkYHa3q8t7K3Sfn_FgM6iHCCT-NCIYLcN",
-		"target_access_key": "jAwOyiesXELlb-lHi3j3zhkp",
-		"target_secret_key": "ydYMBfO0dYcFwDonwGZuwyJ3debrLJMonkL23BNLFZC8mxJZ",
-		"ding": map[string]interface{}{
-			"ding":  "",
-			"phone": "",
-		},
-	}
-	followInfos := []map[string]interface{}{followerInfo}
+	followInfos := []map[string]interface{}{getBm4FollowerInfo(), getBm5FollowerInfo()}
 
 	data, err := json.Marshal(followInfos)
 	if err != nil {
@@ -159,13 +130,7 @@ func startTradeFollower() error {
 }
 
 func stopTradeFollower() error {
-	followerInfo := map[string]interface{}{
-		"target_account": "bm3",
-		"follow_account": "bm5",
-		"exchange":       "bitmex",
-	}
-	followInfos := []map[string]interface{}{followerInfo}
-
+	followInfos := []map[string]interface{}{getBm4FollowerInfo(), getBm5FollowerInfo()}
 	data, err := json.Marshal(followInfos)
 	if err != nil {
 		return err
@@ -192,7 +157,7 @@ func stopTradeFollower() error {
 
 func sendTradeSignalToFollower() {
 	params := map[string]interface{}{
-		"symbol":              "XBTUSD",
+		"symbol":              "ETHUSD",
 		"side":                "Buy",
 		"amount":              int64(1),
 		"timestamp":           time.Now(),
@@ -200,8 +165,8 @@ func sendTradeSignalToFollower() {
 		"exchange":            "bitmex",
 		"order_id":            "6868686868",
 		"order_status":        "Filled",
-		"order_amount":        int64(10),
-		"order_filled_amount": int64(10),
+		"order_amount":        int64(1),
+		"order_filled_amount": int64(1),
 		"order_time":          time.Now(),
 		"order_avg_price":     123.456,
 	}
@@ -226,17 +191,85 @@ func sendTradeSignalToFollower() {
 	_ = sendTask(ctx, task)
 }
 
+func getBm5FollowerInfo() map[string]interface{} {
+	followerInfo := map[string]interface{}{
+		"target_account": "bm3",
+		"follow_account": "bm5",
+		"exchange":       "bitmex",
+		"contracts": []map[string]interface{}{
+			{
+				"id":             "105",
+				"task_title":     "test-bm5",
+				"contract":       "XBTUSD",
+				"buy_limit_max":  "1000",
+				"buy_limit_min":  "0",
+				"sell_limit_max": "1000",
+				"sell_limit_min": "0",
+				"price_offset":   "2",
+				"cancel_time":    "5",
+				"follow_rate":    "1.0",
+			},
+		},
+		"base_host":         "testnet.bitmex.com",
+		"ws_host":           "testnet.bitmex.com",
+		"access_key":        "9U870JJgfMDFo2Vip6xqlIUg",
+		"secret_key":        "9sTMUVJv2KeLkA1FkYHa3q8t7K3Sfn_FgM6iHCCT-NCIYLcN",
+		"target_access_key": "jAwOyiesXELlb-lHi3j3zhkp",
+		"target_secret_key": "ydYMBfO0dYcFwDonwGZuwyJ3debrLJMonkL23BNLFZC8mxJZ",
+		"ding": map[string]interface{}{
+			"ding":   "https://oapi.dingtalk.com/robot/send?access_token=2b23786b0d7661a04c971a2fcde9884c7423fef50c6768e579ba283c7f5d5f1d",
+			"phones": []string{"15988472906"},
+		},
+	}
+
+	return followerInfo
+}
+
+func getBm4FollowerInfo() map[string]interface{} {
+	followerInfo := map[string]interface{}{
+		"target_account": "bm3",
+		"follow_account": "bm4",
+		"exchange":       "bitmex",
+		"contracts": []map[string]interface{}{
+			{
+				"id":             "104",
+				"task_title":     "test-bm4",
+				"contract":       "XBTUSD",
+				"buy_limit_max":  "1000",
+				"buy_limit_min":  "0",
+				"sell_limit_max": "1000",
+				"sell_limit_min": "0",
+				"price_offset":   "2",
+				"cancel_time":    "5",
+				"follow_rate":    "1.0",
+			},
+		},
+		"base_host":         "testnet.bitmex.com",
+		"ws_host":           "testnet.bitmex.com",
+		"access_key":        "F8vOWWaSt2olHd8rUNGnt1gQ",
+		"secret_key":        "15buQgLJDoCaSmXLA9TxZey-9f3S1ybqbbeIj09VGDcBkm55",
+		"target_access_key": "jAwOyiesXELlb-lHi3j3zhkp",
+		"target_secret_key": "ydYMBfO0dYcFwDonwGZuwyJ3debrLJMonkL23BNLFZC8mxJZ",
+		"ding": map[string]interface{}{
+			"ding":   "https://oapi.dingtalk.com/robot/send?access_token=2b23786b0d7661a04c971a2fcde9884c7423fef50c6768e579ba283c7f5d5f1d",
+			"phones": []string{"15988472906"},
+		},
+	}
+
+	return followerInfo
+}
+
 func sendTask(ctx context.Context, signature tasks.Signature) error {
-	_, err := server.SendTaskWithContext(ctx, &signature)
+	asyncResult, err := server.SendTaskWithContext(ctx, &signature)
 	if err != nil {
 		return fmt.Errorf("could not send task: %s", err.Error())
 	}
 	fmt.Println("send task")
 
-	//results, err := asyncResult.Get(time.Millisecond * 5)
-	//if err != nil {
-	//	return fmt.Errorf("getting task result failed with error: %s", err.Error())
-	//}
-	//log.Println(tasks.HumanReadableResults(results))
+	results, err := asyncResult.Get(time.Millisecond * 5)
+	if err != nil {
+		return fmt.Errorf("getting task result failed with error: %s", err.Error())
+	}
+	log.Println(tasks.HumanReadableResults(results))
 	return nil
 }
